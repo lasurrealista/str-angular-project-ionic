5d4c31c2fb04f596ee6fefa7dafd566833071b48
lasurrealista <ladyblue@freemail.hu>
2021-03-10 10:08:19 +0100

    Classes

---
+++ dating-app/src/app/model/connection.ts
@@ -0,0 +1,8 @@
+import { User } from './user';
+
+export class Connection {
+  id: number = 0;
+  user1: User["id"] = 0;
+  user2: User["id"] = 0;
+  accepted: boolean = true;
+}
---
+++ dating-app/src/app/model/user.ts
@@ -0,0 +1,13 @@
+import { Interest } from "./interest";
+import { Location } from "./location"
+
+export class User {
+  id: number = 0;
+  name: string = '';
+  gender: string = '';
+  age: number = 0;
+  photo: string = '';
+  description: string = '';
+  location: Location = new Location();
+  interests: Interest[] = [];
+}
