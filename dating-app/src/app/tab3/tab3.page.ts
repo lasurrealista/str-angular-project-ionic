dab3b428166b6744bbf13e206126e1f4b2e1d0e7
lasurrealista <ladyblue@freemail.hu>
2021-03-12 19:42:46 +0100

    Profile component

--- dating-app/src/app/tab3/tab3.page.ts
+++ dating-app/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-tab3',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+
+  @Input() user = new User();
 
   constructor() {}
 
